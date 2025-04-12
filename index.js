da723ab0372a407b0e4d9de1bb381823935d4aab
TanvirAhmed-1 <tanvirahmed1078@gmail.com>
2025-04-12 12:38:31 +0600

    create review api

--- index.js
+++ index.js
@@ -47,6 +47,12 @@ async function run() {
     res.send(result)
   })
 
+  //get review 
+  app.get('/review',async(req,res)=>{
+    const result=await RestaurantReview.find().toArray();
+    res.send(result)
+  })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
